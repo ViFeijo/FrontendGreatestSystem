3b0b852a81b903d640f4d64a17216ed42603523b
vifeijo <vibfeijo@gmail.com>
2026-05-25 15:43:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  
+
+  return (
+    <div className="bg-orange-100"> 
+      <h1 className="text-3xl font-bold text-gray-900 dark:text-red-500">Jorge</h1>
+    </div>
+  )
+}
+
+export default App
